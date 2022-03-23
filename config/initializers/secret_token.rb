29817ab0947b1dd1daad83f30d69377bd408e824
Carlos <carlos@etica-ti.com.br>
2022-03-23 15:18:13 -0300

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biblioteca::Application.config.secret_token = '149ec738f660e96219c1a9666f7949b9b0b57373d05f86020fae72f0c6798ace80ef3aca9b31e63696a46d6ba7c5c0fd6d80d632e55eb9d343e729c5fd59a6e0'
